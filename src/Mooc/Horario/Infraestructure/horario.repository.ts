98ae9552e002eb93bf0632d3d75c34dba8e7ad88
Sergio Benitez Mombiela <755787@unizar.es>
2022-05-26 19:03:25 +0200

    Devolver GetAvailableHours en orden alfabético y nombre, plan y curso unique al importar asignaturas

--- src/Mooc/Horario/Infraestructure/horario.repository.ts
+++ src/Mooc/Horario/Infraestructure/horario.repository.ts
@@ -12,7 +12,7 @@ import { Inject, Injectable } from "@nestjs/common";
 
 enum HorarioQueries {
     QUERY_TRUNCAR_CURSOS = 'TRUNCATE degree, subject RESTART IDENTITY CASCADE',
-    QUERY_OBTENER_HORAS_DISPONIBLES = 'SELECT nombre, tipo, SUM(duracion) AS duracion, horasestteoria, horasestproblemas, horasestpracticas FROM (SELECT * FROM entry WHERE plan=$1 AND curso=$2 AND grupo=$3) AS entry RIGHT JOIN subject ON entry.nombreasignatura=subject.nombre WHERE subject.plan=$1 AND subject.curso=$2 GROUP BY nombreasignatura,nombre,tipo,horasestteoria,horasestproblemas,horasestpracticas',
+    QUERY_OBTENER_HORAS_DISPONIBLES = 'SELECT nombre, tipo, SUM(duracion) AS duracion, horasestteoria, horasestproblemas, horasestpracticas FROM (SELECT * FROM entry WHERE plan=$1 AND curso=$2 AND grupo=$3) AS entry RIGHT JOIN subject ON entry.nombreasignatura=subject.nombre WHERE subject.plan=$1 AND subject.curso=$2 GROUP BY nombreasignatura,nombre,tipo,horasestteoria,horasestproblemas,horasestpracticas ORDER BY nombre',
     QUERY_CONTAR_TITULACIONES = 'SELECT COUNT(*) FROM degree',
     QUERY_CONTAR_ASIGNATURAS = 'SELECT COUNT(*) FROM subject'
 }
